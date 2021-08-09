131844471048b8927d1bc5a893905433b61c1b87
Amit Jain <amitasn@gmail.com>
2021-08-09 21:14:54 +0530

    feat : updated calculateArea.js

--- calculateArea.js
+++ calculateArea.js
@@ -1,18 +1,63 @@
 const baseInput = document.querySelector("#base");
 const heightInput = document.querySelector("#height");
-const calculateBtn = document.querySelector("#calculate");
+const calculateBtn = document.querySelector("#calculatebh");
 const areaOutputDiv = document.querySelector(".areaOutput");
+const sideInput = document.querySelector("#side");
+const calculateSideBtn = document.querySelector("#calculateSide");
+const sideAreaOutputDiv = document.querySelector(".sideAreaOutput");
 
+areaOutputDiv.style.display="none"
+sideAreaOutputDiv.style.display = "none";
 calculateBtn.addEventListener("click", calculateArea)
 
 function calculateArea(params) {
     
     let  baseValue   = Number(baseInput.value);
     let  heightValue = Number(heightInput.value);
-
     let area = 0;
 
-    area = 1/2 * baseValue * heightValue;
-    areaOutputDiv.innerText = area;
+    if (baseValue <=0 || heightValue <= 0 ) {
+        
+        displayError("Enter the valid inputs")
+    } else {
+        areaOutputDiv.style.display="block"
+        area = 1/2 * baseValue * heightValue;
+        areaOutputDiv.innerText = area;
+    }
+    
 
 }
+
+
+calculateSideBtn.addEventListener("click", calculateSideArea);
+
+function calculateSideArea(params) {
+    
+    let sideInputValue = Number(sideInput.value);
+
+    if (sideInputValue <= 0 ) {
+        displaySideError("Enter the valid inputs");
+    } else {
+        sideAreaOutputDiv.style.display="block";
+        let sqroot = 0;
+        let ans = 0
+        sqroot = Math.sqrt(3);
+        ans = (sqroot/4)*sideInputValue*sideInputValue;
+        sideAreaOutputDiv.innerText = ans
+    }
+}
+
+function displaySideError(msg){
+    sideAreaOutputDiv.style.display="block";
+    sideAreaOutputDiv.innerText = msg;
+}
+
+
+
+
+
+function displayError(msg){
+    areaOutputDiv.style.display="block";
+    areaOutputDiv.innerText = msg;
+}
+
